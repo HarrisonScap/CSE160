f75bcff60c13202ac07ba0bebb67a1cd914dbb6e
Harrison S <hscapell@ucsc.edu>
2024-04-11 01:04:48 -0700

    Updated Program Files -- i think its all working

--- asg0.js
+++ asg0.js
@@ -50,6 +50,18 @@ function handleDrawEvent(){
 	drawVector(v2,"blue");
 }
 
+
+function angleBetween(vector1,vector2){
+	// I implemented this exact function in a godot project 
+	var theta = Math.acos((Vector3.dot(vector1,vector2))/(vector1.magnitude()*vector2.magnitude())) // This is in radians
+	return theta * (180/Math.PI) // Converted to degrees
+}
+
+function areaTriangle(vector1,vector2){
+	return (Vector3.cross(vector1,vector2)).magnitude()/2
+	
+}
+
 function handleDrawOperationEvent(){
 	resetCanvas()
 	let x1 = document.getElementById("x1").value;
@@ -58,9 +70,10 @@ function handleDrawOperationEvent(){
 	let x2 = document.getElementById("x2").value;
 	let y2 = document.getElementById("y2").value;
 
-	var v1 = new Vector3([x1,y1,0])
-	var v2 = new Vector3([x2,y2,0])
-	var v3;
+	var v1 = new Vector3([x1,y1,0]);
+	var v2 = new Vector3([x2,y2,0]);
+	var v3 = new Vector3([0,0,0]);
+	var v4 = new Vector3([0,0,0]);
 
 	drawVector(v1,"red");
 	drawVector(v2,"blue");
@@ -77,13 +90,30 @@ function handleDrawOperationEvent(){
 			break;
 		case "mul":
 			v3 = v1.mul(scalar)
+			v4 = v2.mul(scalar)
 			break;
 		case "div":
 			v3 = v1.div(scalar)
+			v4 = v2.div(scalar)
+			break;
+		case "mag":
+			console.log("Magnitude v1: " + v1.magnitude())
+			console.log("Magnitude v2: " + v2.magnitude())
+			break;
+		case "nor":
+			v3 = v1.normalize()
+			v4 = v2.normalize()
+			break;
+		case "ang":
+			console.log("Angle: " + angleBetween(v1,v2))
+			break;
+		case "tri":
+			console.log("Area: " + areaTriangle(v1,v2))
 			break;
 	}
 
 	drawVector(v3,"green");
+	drawVector(v4,"green");
 
 
 }
